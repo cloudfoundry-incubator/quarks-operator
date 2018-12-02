c31ab0bb1ebb88363c8a928f82af6dcb3e77fe52
viovanov <vlad.iovanov@suse.com>
2018-12-02 21:46:01 +0200

    [#161744859] Implement a controller and reconciler for ExtendedStatefulSet

---
+++ pkg/kube/controllers/extendedstatefulset/controller.go
@@ -0,0 +1,32 @@
+package extendedstatefulset
+
+import (
+	"go.uber.org/zap"
+
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	essv1 "code.cloudfoundry.org/cf-operator/pkg/kube/apis/extendedstatefulsetcontroller/v1alpha1"
+)
+
+// Add creates a new ExtendedStatefulSet controller and adds it to the Manager
+func Add(log *zap.SugaredLogger, mgr manager.Manager) error {
+	r := NewReconciler(log, mgr, controllerutil.SetControllerReference)
+
+	// Create a new controller
+	c, err := controller.New("extendedstatefulset-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource ExtendedStatefulSet
+	err = c.Watch(&source.Kind{Type: &essv1.ExtendedStatefulSet{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
