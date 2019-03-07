b40ca17a6bf764cd0aafaba1a1b56442e8a0b2c3
Zida <shawzida@gmail.com>
2019-03-07 12:12:50 +0800

    Imp variable-interpolation by bosh-cli/director/template package
    [#164357145](https://www.pivotaltracker.com/story/show/164357145)

--- cmd/internal/varinterpolation.go
+++ cmd/internal/varinterpolation.go
@@ -1,9 +1,21 @@
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	boshtpl "github.com/cloudfoundry/bosh-cli/director/template"
+	"github.com/cppforlife/go-patch/patch"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
 )
+
+type Variables map[string]interface{}
 
 // variableInterpolationCmd represents the variableInterpolation command
 var variableInterpolationCmd = &cobra.Command{
@@ -15,9 +27,7 @@ This will interpolate all the variables found in a
 manifest into kubernetes resources.
 
 `,
-	Run: func(cmd *cobra.Command, args []string) {
-		// All flag values should be reachable via the viper.GetString("viper_flag")
-	},
+	RunE: runVariableInterpolationCmd,
 }
 
 func init() {
@@ -31,4 +41,73 @@ func init() {
 
 	viper.BindPFlag("manifest", variableInterpolationCmd.Flags().Lookup("manifest"))
 	viper.BindPFlag("variables_dir", variableInterpolationCmd.Flags().Lookup("variables-dir"))
+}
+
+func runVariableInterpolationCmd(cmd *cobra.Command, args []string) error {
+	defer log.Sync()
+
+	var vars []boshtpl.Variables
+
+	manifestFile := viper.GetString("manifest")
+	variablesDir := viper.GetString("variables_dir")
+
+	if _, err := os.Stat(manifestFile); os.IsNotExist(err) {
+		return errors.Errorf("no such file: %s", manifestFile)
+	}
+
+	info, err := os.Stat(variablesDir)
+	if os.IsNotExist(err) || !info.IsDir() {
+		return errors.Errorf("no such directory: %s", variablesDir)
+	}
+
+	// Read files
+	log.Debugf("Reading manifest file: %s", manifestFile)
+	mBytes, err := ioutil.ReadFile(manifestFile)
+	if err != nil {
+		return errors.Wrapf(err, "could not read manifest file")
+	}
+
+	err = filepath.Walk(variablesDir, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			extension := filepath.Ext(strings.TrimSpace(path))
+			if extension == ".yml" || extension == ".yaml" {
+				log.Debugf("Reading variables file: %s", path)
+				varBytes, err := ioutil.ReadFile(path)
+				if err != nil {
+					log.Fatal(errors.Wrapf(err, "could not read variables file"))
+				}
+				staticVars := boshtpl.StaticVariables{}
+
+				err = yaml.Unmarshal(varBytes, &staticVars)
+				if err != nil {
+					return errors.Wrapf(err, "could not deserialize variables: %s", string(varBytes))
+				}
+
+				vars = append(vars, staticVars)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return errors.Wrapf(err, "could not read variables directory")
+	}
+
+	multiVars := boshtpl.NewMultiVars(vars)
+	tpl := boshtpl.NewTemplate(mBytes)
+
+	// Following options are empty for cf-operator
+	op := patch.Ops{}
+	evalOpts := boshtpl.EvaluateOpts{
+		ExpectAllKeys:     false,
+		ExpectAllVarsUsed: false,
+	}
+
+	bytes, err := tpl.Evaluate(multiVars, op, evalOpts)
+	if err != nil {
+		return errors.Wrapf(err, "could not evaluate variables")
+	}
+
+	fmt.Println("---\n# Manifest:")
+	fmt.Println(string(bytes))
+	return nil
 }
