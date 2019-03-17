b3e1dadae47d7c51f58bcd0930639fc5199254b9
Sakala Venkata Krishna Rohit <rohit.sakala@suse.com>
2019-03-17 12:53:57 +0530

    increase timeout

--- integration/environment/environment.go
+++ integration/environment/environment.go
@@ -52,7 +52,7 @@ func NewEnvironment() *Environment {
 			Fs:         afero.NewOsFs(),
 		},
 		Machine: Machine{
-			pollTimeout:  30 * time.Second,
+			pollTimeout:  150 * time.Second,
 			pollInterval: 500 * time.Millisecond,
 		},
 	}
