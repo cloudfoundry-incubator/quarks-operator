79ab07ab474aed3001ead5eebbad8745d06bddf5
viovanov <vlad.iovanov@suse.com>
2019-03-20 14:19:42 +0200

    Remove storage class test helpers

--- integration/suite_test.go
+++ integration/suite_test.go
@@ -1,12 +1,10 @@
 package integration_test
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
 	"code.cloudfoundry.org/cf-operator/integration/environment"
-	operatortesting "code.cloudfoundry.org/cf-operator/testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -18,10 +16,8 @@ func TestIntegration(t *testing.T) {
 }
 
 var (
-	env                  *environment.Environment
-	stopOperator         environment.StopFunc
-	storageClassTeardown environment.TearDownFunc
-	storageClassName     string
+	env          *environment.Environment
+	stopOperator environment.StopFunc
 )
 
 var _ = BeforeSuite(func() {
@@ -30,16 +26,9 @@ var _ = BeforeSuite(func() {
 	var err error
 	stopOperator, err = env.Setup()
 	Expect(err).NotTo(HaveOccurred())
-
-	storageClassName = fmt.Sprintf("testsc-%s", operatortesting.RandString(5))
-	storageClass := env.DefaultStorageClass(storageClassName)
-	_, storageClassTeardown, err = env.CreateStorageClass(storageClass)
-	Expect(err).NotTo(HaveOccurred())
 })
 
 var _ = AfterSuite(func() {
-	defer func(tdf environment.TearDownFunc) { Expect(tdf()).To(Succeed()) }(storageClassTeardown)
-
 	if stopOperator != nil {
 		time.Sleep(3 * time.Second)
 		defer stopOperator()
