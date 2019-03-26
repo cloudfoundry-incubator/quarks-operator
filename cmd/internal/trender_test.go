0bb0e1a354bb32b65b21fbde16513548643fb742
André Duffeck <aduffeck@suse.com>
2019-03-26 15:12:44 +0100

    Extract job instance information from the resolved properties manifest

--- cmd/internal/trender_test.go
+++ cmd/internal/trender_test.go
@@ -6,7 +6,6 @@ import (
 	"path/filepath"
 
 	. "code.cloudfoundry.org/cf-operator/cmd/internal"
-	"code.cloudfoundry.org/cf-operator/pkg/bosh/manifest"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	yaml "gopkg.in/yaml.v2"
@@ -20,60 +19,56 @@ var _ = Describe("Trender", func() {
 			deploymentManifest string
 			jobsDir            string
 			instanceGroupName  string
-			address            string
-			az                 string
-			id                 string
 			index              int
-			ip                 string
-			name               string
-			jobInstance        manifest.JobInstance
 		)
 
 		BeforeEach(func() {
 			deploymentManifest = "../../testing/assets/gatherManifest.yml"
 			jobsDir = "../../testing/assets"
 			instanceGroupName = "log-api"
-			address = ""
-			az = "z1"
-			id = "doppler-0-doppler"
-			index = 0
-			ip = ""
-			name = "doppler-doppler"
-
-			jobInstance = manifest.JobInstance{
-				Address: address,
-				AZ:      az,
-				ID:      id,
-				Index:   index,
-				Name:    name,
-				IP:      ip,
-			}
 		})
 
-		It("should render the job erb files correctly", func() {
+		Context("with an invalid instance index", func() {
+			BeforeEach(func() {
+				index = 1000
+			})
 
-			result := RenderData(deploymentManifest, jobsDir, instanceGroupName, jobInstance)
-			Expect(result).To(BeNil())
+			It("fails", func() {
+				err := RenderData(deploymentManifest, jobsDir, instanceGroupName, index)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("no instance found"))
+			})
+		})
 
-			absDestFile := filepath.Join(jobsDir, "loggregator_trafficcontroller", "config/bpm.yml")
-			Expect(absDestFile).Should(BeAnExistingFile())
+		Context("with a valid instance index", func() {
+			BeforeEach(func() {
+				index = 0
+			})
 
-			// Unmarshal the rendered file
-			bpmYmlBytes, err := ioutil.ReadFile(absDestFile)
-			Expect(err).ToNot(HaveOccurred())
+			It("renders the job erb files correctly", func() {
+				err := RenderData(deploymentManifest, jobsDir, instanceGroupName, index)
+				Expect(err).ToNot(HaveOccurred())
 
-			var bpmYml map[string][]interface{}
-			err = yaml.Unmarshal(bpmYmlBytes, &bpmYml)
-			Expect(err).ToNot(HaveOccurred())
+				absDestFile := filepath.Join(jobsDir, "loggregator_trafficcontroller", "config/bpm.yml")
+				Expect(absDestFile).Should(BeAnExistingFile())
 
-			// Check fields if they are rendered
-			Expect(bpmYml["processes"][0].(map[interface{}]interface{})["env"].(map[interface{}]interface{})["AGENT_UDP_ADDRESS"]).To(Equal("127.0.0.1:3457"))
-			Expect(bpmYml["processes"][0].(map[interface{}]interface{})["env"].(map[interface{}]interface{})["TRAFFIC_CONTROLLER_OUTGOING_DROPSONDE_PORT"]).To(Equal("8081"))
-			Expect(bpmYml["processes"][0].(map[interface{}]interface{})["env"].(map[interface{}]interface{})["FOOBARWITHLINKINSTANCESAZ"]).To(Equal("z1"))
+				// Unmarshal the rendered file
+				bpmYmlBytes, err := ioutil.ReadFile(absDestFile)
+				Expect(err).ToNot(HaveOccurred())
 
-			// Delete  the file
-			err = os.RemoveAll(filepath.Join(jobsDir, "loggregator_trafficcontroller"))
-			Expect(err).NotTo(HaveOccurred())
+				var bpmYml map[string][]interface{}
+				err = yaml.Unmarshal(bpmYmlBytes, &bpmYml)
+				Expect(err).ToNot(HaveOccurred())
+
+				// Check fields if they are rendered
+				Expect(bpmYml["processes"][0].(map[interface{}]interface{})["env"].(map[interface{}]interface{})["AGENT_UDP_ADDRESS"]).To(Equal("127.0.0.1:3457"))
+				Expect(bpmYml["processes"][0].(map[interface{}]interface{})["env"].(map[interface{}]interface{})["TRAFFIC_CONTROLLER_OUTGOING_DROPSONDE_PORT"]).To(Equal("8081"))
+				Expect(bpmYml["processes"][0].(map[interface{}]interface{})["env"].(map[interface{}]interface{})["FOOBARWITHLINKINSTANCESAZ"]).To(Equal("z1"))
+
+				// Delete  the file
+				err = os.RemoveAll(filepath.Join(jobsDir, "loggregator_trafficcontroller"))
+				Expect(err).NotTo(HaveOccurred())
+			})
 		})
 	})
 })
