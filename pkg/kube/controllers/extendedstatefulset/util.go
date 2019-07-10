4b64fe2fc87c59565b25f58843ca49c6bb9430f1
Zida <shawzida@gmail.com>
2019-07-10 15:26:47 +0800

    Only process create event of ExtendedSecret which have no generated secret

--- pkg/kube/controllers/extendedstatefulset/util.go
+++ pkg/kube/controllers/extendedstatefulset/util.go
@@ -8,7 +8,6 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	crc "sigs.k8s.io/controller-runtime/pkg/client"
 
-	bdm "code.cloudfoundry.org/cf-operator/pkg/bosh/manifest"
 	estsv1 "code.cloudfoundry.org/cf-operator/pkg/kube/apis/extendedstatefulset/v1alpha1"
 	"code.cloudfoundry.org/cf-operator/pkg/kube/util/ctxlog"
 )
@@ -17,21 +16,15 @@ import (
 func listStatefulSets(ctx context.Context, client crc.Client, exStatefulSet *estsv1.ExtendedStatefulSet) ([]v1beta2.StatefulSet, error) {
 	ctxlog.Debug(ctx, "Listing StatefulSets owned by ExtendedStatefulSet '", exStatefulSet.Name, "'.")
 
-	secretLabelsSet := labels.Set{
-		bdm.LabelDeploymentName:    exStatefulSet.Spec.Template.Labels[bdm.LabelDeploymentName],
-		bdm.LabelInstanceGroupName: exStatefulSet.Spec.Template.Labels[bdm.LabelInstanceGroupName],
-	}
-
-	result := []v1beta2.StatefulSet{}
-
 	// Get owned resources
 	// Go through each StatefulSet
+	result := []v1beta2.StatefulSet{}
 	allStatefulSets := &v1beta2.StatefulSetList{}
 	err := client.List(
 		ctx,
 		&crc.ListOptions{
 			Namespace:     exStatefulSet.Namespace,
-			LabelSelector: secretLabelsSet.AsSelector(),
+			LabelSelector: labels.Everything(),
 		},
 		allStatefulSets)
 	if err != nil {
