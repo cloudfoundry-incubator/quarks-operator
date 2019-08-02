dc2059c15ed09e6fff93949418137b6657c1951b
Thulio Ferraz Assis <tassis@suse.com>
2019-08-02 14:26:08 -0700

    Avoid reusing vars on extendedjob package

--- pkg/kube/controllers/extendedjob/errand_reconciler.go
+++ pkg/kube/controllers/extendedjob/errand_reconciler.go
@@ -18,7 +18,7 @@ import (
 
 var _ reconcile.Reconciler = &ErrandReconciler{}
 
-// NewErrandReconciler returns a new reconciler for errand jobs
+// NewErrandReconciler returns a new reconciler for errand jobs.
 func NewErrandReconciler(
 	ctx context.Context,
 	config *config.Config,
@@ -38,7 +38,7 @@ func NewErrandReconciler(
 	}
 }
 
-// ErrandReconciler implements the Reconciler interface
+// ErrandReconciler implements the Reconciler interface.
 type ErrandReconciler struct {
 	ctx               context.Context
 	client            client.Client
@@ -48,9 +48,8 @@ type ErrandReconciler struct {
 	jobCreator        JobCreator
 }
 
-// Reconcile starts jobs for extended jobs of the type errand with Run being set to 'now' manually
+// Reconcile starts jobs for extended jobs of the type errand with Run being set to 'now' manually.
 func (r *ErrandReconciler) Reconcile(request reconcile.Request) (reconcile.Result, error) {
-	var result = reconcile.Result{}
 	eJob := &ejv1.ExtendedJob{}
 
 	// Set the ctx to be Background, as the top-level context for incoming requests.
@@ -58,52 +57,45 @@ func (r *ErrandReconciler) Reconcile(request reconcile.Request) (reconcile.Resul
 	defer cancel()
 
 	ctxlog.Info(ctx, "Reconciling errand job ", request.NamespacedName)
-	err := r.client.Get(ctx, request.NamespacedName, eJob)
-	if err != nil {
+	if err := r.client.Get(ctx, request.NamespacedName, eJob); err != nil {
 		if apierrors.IsNotFound(err) {
-			// do not requeue, extended job is probably deleted
+			// Do not requeue, extended job is probably deleted.
 			ctxlog.Infof(ctx, "Failed to find extended job '%s', not retrying: %s", request.NamespacedName, err)
-			err = nil
-			return result, err
+			return reconcile.Result{}, nil
 		}
 		// Error reading the object - requeue the request.
 		ctxlog.Errorf(ctx, "Failed to get the extended job '%s': %s", request.NamespacedName, err)
-		return result, err
+		return reconcile.Result{}, err
 	}
 
 	if eJob.Spec.Trigger.Strategy == ejv1.TriggerNow {
-		// set Strategy back to manual for errand jobs
+		// Set Strategy back to manual for errand jobs.
 		eJob.Spec.Trigger.Strategy = ejv1.TriggerManual
-		err = r.client.Update(ctx, eJob)
-		if err != nil {
-			err = ctxlog.WithEvent(eJob, "UpdateError").Errorf(ctx, "Failed to revert to 'trigger.strategy=manual' on job '%s': %s", eJob.Name, err)
-			return result, err
+		if err := r.client.Update(ctx, eJob); err != nil {
+			return reconcile.Result{}, ctxlog.WithEvent(eJob, "UpdateError").Errorf(ctx, "Failed to revert to 'trigger.strategy=manual' on job '%s': %s", eJob.Name, err)
 		}
 	}
 
-	retry, err := r.jobCreator.createJob(ctx, *eJob, "", "")
-	if err != nil {
-		err = ctxlog.WithEvent(eJob, "CreateJobError").Errorf(ctx, "Failed to create job '%s': %s", eJob.Name, err)
-		return result, err
-	}
-	if retry {
+	if retry, err := r.jobCreator.Create(ctx, *eJob, "", ""); err != nil {
+		return reconcile.Result{}, ctxlog.WithEvent(eJob, "CreateJobError").Errorf(ctx, "Failed to create job '%s': %s", eJob.Name, err)
+	} else if retry {
 		ctxlog.Infof(ctx, "Retrying to create job '%s'", eJob.Name)
-		result.Requeue = true
-		result.RequeueAfter = time.Second * 5
+		result := reconcile.Result{
+			Requeue:      true,
+			RequeueAfter: time.Second * 5,
+		}
 		return result, nil
 	}
 
 	ctxlog.WithEvent(eJob, "CreateJob").Infof(ctx, "Created errand job for '%s'", eJob.Name)
 
 	if eJob.Spec.Trigger.Strategy == ejv1.TriggerOnce {
-		// traverse Strategy into the final 'done' state
+		// Traverse Strategy into the final 'done' state.
 		eJob.Spec.Trigger.Strategy = ejv1.TriggerDone
-		err = r.client.Update(ctx, eJob)
-		if err != nil {
-			err = ctxlog.WithEvent(eJob, "UpdateError").Errorf(ctx, "Failed to traverse to 'trigger.strategy=done' on job '%s': %s", eJob.Name, err)
-			return reconcile.Result{Requeue: false}, err
+		if err := r.client.Update(ctx, eJob); err != nil {
+			return reconcile.Result{Requeue: false}, ctxlog.WithEvent(eJob, "UpdateError").Errorf(ctx, "Failed to traverse to 'trigger.strategy=done' on job '%s': %s", eJob.Name, err)
 		}
 	}
 
-	return result, err
+	return reconcile.Result{}, nil
 }
