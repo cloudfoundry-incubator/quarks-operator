194e141e794b652c62b6a437dc772b21e3cd5b2e
Thulio Ferraz Assis <tassis@suse.com>
2019-08-23 13:26:10 -0700

    Added tests for container-run

---
+++ container-run/pkg/containerrun/cmd_test.go
@@ -0,0 +1,68 @@
+package containerrun_test
+
+import (
+	"fmt"
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "code.cloudfoundry.org/cf-operator/container-run/pkg/containerrun"
+)
+
+var _ = Describe("NewContainerRunCmd", func() {
+	It("constructs a new command", func() {
+		cmd := NewContainerRunCmd(nil, nil, nil, Stdio{})
+		Expect(cmd).ToNot(Equal(nil))
+	})
+
+	It("fails when the run argument returns an error", func() {
+		expectedErr := fmt.Errorf("failed")
+		run := func(
+			_ Runner,
+			_ Runner,
+			_ Stdio,
+			_ []string,
+			_ string,
+			_ []string,
+			_ string,
+			_ []string,
+		) error {
+			return expectedErr
+		}
+		cmd := NewContainerRunCmd(run, nil, nil, Stdio{})
+		origArgs := os.Args[:]
+		os.Args = os.Args[:1]
+		err := cmd.Execute()
+		os.Args = origArgs[:]
+		Expect(err).To(Equal(expectedErr))
+	})
+
+	It("succeeds when the run argument returns no error", func() {
+		run := func(
+			_ Runner,
+			_ Runner,
+			_ Stdio,
+			_ []string,
+			_ string,
+			_ []string,
+			_ string,
+			_ []string,
+		) error {
+			return nil
+		}
+		cmd := NewContainerRunCmd(run, nil, nil, Stdio{})
+		origArgs := os.Args[:]
+		os.Args = os.Args[:1]
+		err := cmd.Execute()
+		os.Args = origArgs[:]
+		Expect(err).To(BeNil())
+	})
+})
+
+var _ = Describe("NewDefaultContainerRunCmd", func() {
+	It("constructs a new command", func() {
+		cmd := NewDefaultContainerRunCmd()
+		Expect(cmd).ToNot(Equal(nil))
+	})
+})
---
+++ container-run/pkg/containerrun/suite_test.go
@@ -0,0 +1,13 @@
+package containerrun_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestContainerrun(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "container-run Suite")
+}
