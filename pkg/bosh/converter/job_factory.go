d034d0831aed0d1307a6b1b06d3083a79f1191e7
Zida <shawzida@gmail.com>
2019-09-04 11:37:21 +0800

    Provide job factory interface - Refactor EJob factory signatures - Remove job label of manifest SHA1 - Add jobFactory unit cases for deployment reconciler

--- pkg/bosh/converter/job_factory.go
+++ pkg/bosh/converter/job_factory.go
@@ -38,28 +38,30 @@ const (
 )
 
 // JobFactory creates Jobs for a given manifest
-type JobFactory struct {
-	Manifest            bdm.Manifest
-	Namespace           string
-	desiredManifestName string
+type JobFactory interface {
+	VariableInterpolationJob(manifest bdm.Manifest) (*ejv1.ExtendedJob, error)
+	InstanceGroupManifestJob(manifest bdm.Manifest) (*ejv1.ExtendedJob, error)
+	BPMConfigsJob(manifest bdm.Manifest) (*ejv1.ExtendedJob, error)
 }
 
-// NewJobFactory returns a new JobFactory
-func NewJobFactory(manifest bdm.Manifest, namespace string) *JobFactory {
-	return &JobFactory{
-		Manifest:  manifest,
+// ConcreteJobFactory a concrete implementation of JobFactory
+type ConcreteJobFactory struct {
+	Namespace string
+}
+
+// NewConcreteJobFactory returns a concrete implementation of JobFactory
+func NewConcreteJobFactory(namespace string) JobFactory {
+	return &ConcreteJobFactory{
 		Namespace: namespace,
-		// ExtendedJob will always pick the latest version for versioned secrets
-		desiredManifestName: names.DesiredManifestName(manifest.Name, "1"),
 	}
 }
 
 // VariableInterpolationJob returns an extended job to interpolate variables
-func (f *JobFactory) VariableInterpolationJob() (*ejv1.ExtendedJob, error) {
+func (f *ConcreteJobFactory) VariableInterpolationJob(manifest bdm.Manifest) (*ejv1.ExtendedJob, error) {
 	args := []string{"util", "variable-interpolation"}
 
 	// This is the source manifest, that still has the '((vars))'
-	manifestSecretName := names.CalculateSecretName(names.DeploymentSecretTypeManifestWithOps, f.Manifest.Name, "")
+	manifestSecretName := names.CalculateSecretName(names.DeploymentSecretTypeManifestWithOps, manifest.Name, "")
 
 	// Prepare Volumes and Volume mounts
 
@@ -67,28 +69,21 @@ func (f *JobFactory) VariableInterpolationJob() (*ejv1.ExtendedJob, error) {
 	volumeMounts := []corev1.VolumeMount{withOpsVolumeMount(manifestSecretName)}
 
 	// We need a volume and a mount for each input variable
-	for _, variable := range f.Manifest.Variables {
+	for _, variable := range manifest.Variables {
 		varName := variable.Name
-		varSecretName := names.CalculateSecretName(names.DeploymentSecretTypeVariable, f.Manifest.Name, varName)
+		varSecretName := names.CalculateSecretName(names.DeploymentSecretTypeVariable, manifest.Name, varName)
 
 		volumes = append(volumes, variableVolume(varSecretName))
-
 		volumeMounts = append(volumeMounts, variableVolumeMount(varSecretName, varName))
 	}
 
 	// If there are no variables, mount an empty dir for variables
-	if len(f.Manifest.Variables) == 0 {
+	if len(manifest.Variables) == 0 {
 		volumes = append(volumes, noVarsVolume())
 		volumeMounts = append(volumeMounts, noVarsVolumeMount())
 	}
 
-	// Calculate the signature of the manifest, to label things
-	manifestSignature, err := f.Manifest.SHA1()
-	if err != nil {
-		return nil, errors.Wrapf(err, "Generation of Variable interpolation job failed.")
-	}
-
-	eJobName := fmt.Sprintf("dm-%s", f.Manifest.Name)
+	eJobName := fmt.Sprintf("dm-%s", manifest.Name)
 
 	// Construct the var interpolation auto-errand eJob
 	job := &ejv1.ExtendedJob{
@@ -96,17 +91,16 @@ func (f *JobFactory) VariableInterpolationJob() (*ejv1.ExtendedJob, error) {
 			Name:      eJobName,
 			Namespace: f.Namespace,
 			Labels: map[string]string{
-				bdv1.LabelDeploymentName: f.Manifest.Name,
+				bdv1.LabelDeploymentName: manifest.Name,
 			},
 		},
 		Spec: ejv1.ExtendedJobSpec{
 			Output: &ejv1.Output{
-				NamePrefix: names.DesiredManifestPrefix(f.Manifest.Name),
+				NamePrefix: names.DesiredManifestPrefix(manifest.Name),
 				SecretLabels: map[string]string{
-					bdv1.LabelDeploymentName:       f.Manifest.Name,
-					bdv1.LabelManifestSHA1:         manifestSignature,
+					bdv1.LabelDeploymentName:       manifest.Name,
 					bdv1.LabelDeploymentSecretType: names.DeploymentSecretTypeManifestWithOps.String(),
-					ejv1.LabelReferencedJobName:    fmt.Sprintf("instance-group-%s", f.Manifest.Name),
+					ejv1.LabelReferencedJobName:    fmt.Sprintf("instance-group-%s", manifest.Name),
 				},
 				Versioned: true,
 			},
@@ -150,40 +144,44 @@ func (f *JobFactory) VariableInterpolationJob() (*ejv1.ExtendedJob, error) {
 }
 
 // InstanceGroupManifestJob generates the job to create an instance group manifest
-func (f *JobFactory) InstanceGroupManifestJob() (*ejv1.ExtendedJob, error) {
-	containers := make([]corev1.Container, len(f.Manifest.InstanceGroups))
+func (f *ConcreteJobFactory) InstanceGroupManifestJob(manifest bdm.Manifest) (*ejv1.ExtendedJob, error) {
+	containers := make([]corev1.Container, len(manifest.InstanceGroups))
 
-	for idx, ig := range f.Manifest.InstanceGroups {
+	// ExtendedJob will always pick the latest version for versioned secrets
+	desiredManifestName := names.DesiredManifestName(manifest.Name, "1")
+
+	for idx, ig := range manifest.InstanceGroups {
 		// One container per Instance Group
 		// There will be one secret generated for each of these containers
-		containers[idx] = f.gatheringContainer("instance-group", ig.Name)
+		containers[idx] = f.gatheringContainer("instance-group", desiredManifestName, ig.Name)
 	}
 
-	eJobName := fmt.Sprintf("ig-%s", f.Manifest.Name)
-	return f.gatheringJob(eJobName, names.DeploymentSecretTypeInstanceGroupResolvedProperties, containers)
+	eJobName := fmt.Sprintf("ig-%s", manifest.Name)
+	return f.gatheringJob(eJobName, manifest, desiredManifestName, names.DeploymentSecretTypeInstanceGroupResolvedProperties, containers)
 }
 
 // BPMConfigsJob returns an extended job to calculate BPM information
-func (f *JobFactory) BPMConfigsJob() (*ejv1.ExtendedJob, error) {
-	containers := make([]corev1.Container, len(f.Manifest.InstanceGroups))
+func (f *ConcreteJobFactory) BPMConfigsJob(manifest bdm.Manifest) (*ejv1.ExtendedJob, error) {
+	containers := make([]corev1.Container, len(manifest.InstanceGroups))
+	desiredManifestName := names.DesiredManifestName(manifest.Name, "1")
 
-	for idx, ig := range f.Manifest.InstanceGroups {
+	for idx, ig := range manifest.InstanceGroups {
 		// One container per Instance Group
 		// There will be one secret generated for each of these containers
-		containers[idx] = f.gatheringContainer("bpm-configs", ig.Name)
+		containers[idx] = f.gatheringContainer("bpm-configs", desiredManifestName, ig.Name)
 	}
 
-	eJobName := fmt.Sprintf("bpm-%s", f.Manifest.Name)
-	return f.gatheringJob(eJobName, names.DeploymentSecretBpmInformation, containers)
+	eJobName := fmt.Sprintf("bpm-%s", manifest.Name)
+	return f.gatheringJob(eJobName, manifest, desiredManifestName, names.DeploymentSecretBpmInformation, containers)
 }
 
-func (f *JobFactory) gatheringContainer(cmd, instanceGroupName string) corev1.Container {
+func (f *ConcreteJobFactory) gatheringContainer(cmd, desiredManifestName string, instanceGroupName string) corev1.Container {
 	return corev1.Container{
 		Name:  names.Sanitize(instanceGroupName),
 		Image: GetOperatorDockerImage(),
 		Args:  []string{"util", cmd},
 		VolumeMounts: []corev1.VolumeMount{
-			withOpsVolumeMount(f.desiredManifestName),
+			withOpsVolumeMount(desiredManifestName),
 			releaseSourceVolumeMount(),
 		},
 		Env: []corev1.EnvVar{
@@ -207,12 +205,12 @@ func (f *JobFactory) gatheringContainer(cmd, instanceGroupName string) corev1.Co
 	}
 }
 
-func (f *JobFactory) gatheringJob(name string, secretType names.DeploymentSecretType, containers []corev1.Container) (*ejv1.ExtendedJob, error) {
-	outputSecretNamePrefix := names.CalculateIGSecretPrefix(secretType, f.Manifest.Name)
+func (f *ConcreteJobFactory) gatheringJob(name string, manifest bdm.Manifest, desiredManifestName string, secretType names.DeploymentSecretType, containers []corev1.Container) (*ejv1.ExtendedJob, error) {
+	outputSecretNamePrefix := names.CalculateIGSecretPrefix(secretType, manifest.Name)
 
 	initContainers := []corev1.Container{}
 	doneSpecCopyingReleases := map[string]bool{}
-	for _, ig := range f.Manifest.InstanceGroups {
+	for _, ig := range manifest.InstanceGroups {
 		// Iterate through each Job to find all releases so we can copy all
 		// sources to /var/vcap/instance-group
 		for _, boshJob := range ig.Jobs {
@@ -224,9 +222,9 @@ func (f *JobFactory) gatheringJob(name string, secretType names.DeploymentSecret
 			doneSpecCopyingReleases[releaseName] = true
 
 			// Get the docker image for the release
-			releaseImage, err := (&f.Manifest).GetReleaseImage(ig.Name, boshJob.Name)
+			releaseImage, err := (&manifest).GetReleaseImage(ig.Name, boshJob.Name)
 			if err != nil {
-				return nil, errors.Wrapf(err, "Generation of gathering job failed for manifest %s", f.Manifest.Name)
+				return nil, errors.Wrapf(err, "Generation of gathering job failed for manifest %s", manifest.Name)
 			}
 			// Create an init container that copies sources
 			// TODO: destination should also contain release name, to prevent overwrites
@@ -240,14 +238,14 @@ func (f *JobFactory) gatheringJob(name string, secretType names.DeploymentSecret
 			Name:      name,
 			Namespace: f.Namespace,
 			Labels: map[string]string{
-				bdv1.LabelDeploymentName: f.Manifest.Name,
+				bdv1.LabelDeploymentName: manifest.Name,
 			},
 		},
 		Spec: ejv1.ExtendedJobSpec{
 			Output: &ejv1.Output{
 				NamePrefix: outputSecretNamePrefix,
 				SecretLabels: map[string]string{
-					bdv1.LabelDeploymentName:       f.Manifest.Name,
+					bdv1.LabelDeploymentName:       manifest.Name,
 					bdv1.LabelDeploymentSecretType: secretType.String(),
 				},
 				Versioned: true,
@@ -271,7 +269,7 @@ func (f *JobFactory) gatheringJob(name string, secretType names.DeploymentSecret
 					Containers: containers,
 					// Volumes for secrets
 					Volumes: []corev1.Volume{
-						*withOpsVolume(f.desiredManifestName),
+						*withOpsVolume(desiredManifestName),
 						releaseSourceVolume(),
 					},
 				},
