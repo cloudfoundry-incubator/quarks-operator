e7b2238a95f510b6dc616176918b750d8fe5c70d
André Duffeck <aduffeck@suse.com>
2019-09-18 10:27:22 +0200

    Add remaining check for bpm process

--- pkg/bosh/converter/bpm_resources_test.go
+++ pkg/bosh/converter/bpm_resources_test.go
@@ -321,7 +321,8 @@ var _ = Describe("kube converter", func() {
 				Expect(err).ShouldNot(HaveOccurred())
 				Expect(resources.Errands).To(HaveLen(1))
 
-				_, err = act(bpmConfigs[1], m.InstanceGroups[1])
+				resources, err = act(bpmConfigs[1], m.InstanceGroups[1])
+				Expect(resources.InstanceGroups).To(HaveLen(1))
 				Expect(err).ShouldNot(HaveOccurred())
 
 				resources, err = act(bpmConfigs[2], m.InstanceGroups[2])
