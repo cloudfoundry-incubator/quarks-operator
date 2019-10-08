0082ab2dfe6564fb894df125cdf3c6e7323b76ef
Sakala Venkata Krishna Rohit <rohitsakala@gmail.com>
2019-10-08 16:56:10 +0200

    Refactor persistoutput and create unit tests

--- pkg/kube/controllers/extendedjob/job_create_secret.go
+++ pkg/kube/controllers/extendedjob/job_create_secret.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
 	"regexp"
 	"strconv"
 
@@ -17,7 +18,6 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 
 	"code.cloudfoundry.org/cf-operator/pkg/bosh/converter"
 	"code.cloudfoundry.org/cf-operator/pkg/kube/apis"
@@ -33,51 +33,60 @@ var (
 	LabelEjobPod = fmt.Sprintf("%s/ejob-name", apis.GroupName)
 )
 
+// PersistOutputInterface creates a kubernetes secret for each container in the in the extendedjob pod.
+type PersistOutputInterface struct {
+	namespace            string
+	podName              string
+	clientSet            kubernetes.Interface
+	versionedClientSet   versioned.Interface
+	outputFilePathPrefix string
+}
+
+// NewPersistOutputInterface returns a persistoutput interface which can create kubernetes secrets.
+func NewPersistOutputInterface(namespace string, podName string, clientSet kubernetes.Interface, versionedClientSet versioned.Interface, outputFilePathPrefix string) *PersistOutputInterface {
+	return &PersistOutputInterface{
+		namespace:            namespace,
+		podName:              podName,
+		clientSet:            clientSet,
+		versionedClientSet:   versionedClientSet,
+		outputFilePathPrefix: outputFilePathPrefix,
+	}
+}
+
 // PersistOutput converts the output files of each container
 // in the pod related to an ejob into a kubernetes secret.
-func PersistOutput(namespace string) error {
-
-	// hostname of the container is the pod name in kubernetes
-	podName, err := os.Hostname()
-	if err != nil {
-		return errors.Wrapf(err, "failed to fetch pod name.")
-	}
-	if podName == "" {
-		return errors.Wrapf(err, "pod name is empty.")
-	}
-
-	// Authenticate with the cluster
-	clientSet, versionedClientSet, err := authenticateInCluster()
-	if err != nil {
-		return err
-	}
+func (po *PersistOutputInterface) PersistOutput() error {
 
 	// Fetch the pod
-	pod, err := clientSet.CoreV1().Pods(namespace).Get(podName, metav1.GetOptions{})
+	pod, err := po.clientSet.CoreV1().Pods(po.namespace).Get(po.podName, metav1.GetOptions{})
 	if err != nil {
-		return errors.Wrapf(err, "failed to fetch pod %s", podName)
+		return errors.Wrapf(err, "failed to fetch pod %s", po.podName)
 	}
 
 	// Fetch the exjob
 	exjobName := pod.GetLabels()[LabelEjobPod]
 
-	exjobClient := versionedClientSet.ExtendedjobV1alpha1().ExtendedJobs(namespace)
+	exjobClient := po.versionedClientSet.ExtendedjobV1alpha1().ExtendedJobs(po.namespace)
 	exjob, err := exjobClient.Get(exjobName, metav1.GetOptions{})
 	if err != nil {
 		return errors.Wrapf(err, "failed to fetch exjob")
 	}
 
-	// convert output if needed
-	if exjob.Spec.Output != nil {
-		err = convertOutputToSecret(pod, exjob.Spec.Output.NamePrefix, namespace, clientSet, exjob)
+	// Persist output if needed
+	if !reflect.DeepEqual(ejv1.Output{}, exjob.Spec.Output) && exjob.Spec.Output != nil {
+		err = po.ConvertOutputToSecret(pod, exjob)
 		if err != nil {
 			return err
+
 		}
 	}
 	return nil
 }
 
-func convertOutputToSecret(pod *corev1.Pod, namePrefix string, namespace string, clientSet *kubernetes.Clientset, exjob *ejv1.ExtendedJob) error {
+// ConvertOutputToSecret waits for outputfile create event
+// and creates a secret as the event is fired.
+func (po *PersistOutputInterface) ConvertOutputToSecret(pod *corev1.Pod, exjob *ejv1.ExtendedJob) error {
+
 	fileNotifyChannel := make(chan int)
 	errorChannel := make(chan error)
 
@@ -88,10 +97,10 @@ func convertOutputToSecret(pod *corev1.Pod, namePrefix string, namespace string,
 			continue
 		}
 
-		filePath := filepath.Join("/mnt/quarks/", container.Name, "output.json")
+		filePath := filepath.Join(po.outputFilePathPrefix, container.Name, "output.json")
 
 		// Go routine to wait for the file to be created
-		go waitForFile(containerIndex, filePath, fileNotifyChannel, errorChannel, container.Name)
+		go waitForFile(containerIndex, filePath, fileNotifyChannel, errorChannel, container.Name, po.outputFilePathPrefix)
 	}
 
 	// wait for all the go routines
@@ -99,7 +108,7 @@ func convertOutputToSecret(pod *corev1.Pod, namePrefix string, namespace string,
 		select {
 		case containerIndex := <-fileNotifyChannel:
 			outputContainer := pod.Spec.Containers[containerIndex]
-			err := createOutputSecret(outputContainer, namePrefix, namespace, pod.Name, clientSet, exjob)
+			err := po.CreateSecret(outputContainer, pod.Name, exjob)
 			if err != nil {
 				return err
 			}
@@ -108,6 +117,111 @@ func convertOutputToSecret(pod *corev1.Pod, namePrefix string, namespace string,
 		}
 	}
 	return nil
+}
+
+// CreateSecret converts the output file into json and creates a secret for a given container
+func (po *PersistOutputInterface) CreateSecret(outputContainer corev1.Container, podName string, exjob *ejv1.ExtendedJob) error {
+
+	namePrefix := exjob.Spec.Output.NamePrefix
+	secretName := namePrefix + outputContainer.Name
+
+	// Fetch json from file
+	filePath := filepath.Join(po.outputFilePathPrefix, outputContainer.Name, "output.json")
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return errors.Wrapf(err, "unable to read file %s in container %s in pod %s", filePath, outputContainer.Name, podName)
+	}
+	var data map[string]string
+	err = json.Unmarshal([]byte(file), &data)
+	if err != nil {
+		return errors.Wrapf(err, "failed to convert output file %s into json for creating secret %s in pod %s",
+			filePath, secretName, podName)
+	}
+
+	// Create secret for the outputfile to persist
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      secretName,
+			Namespace: po.namespace,
+		},
+	}
+	secretLabels := exjob.Spec.Output.SecretLabels
+	if secretLabels == nil {
+		secretLabels = map[string]string{}
+	}
+	secretLabels[ejv1.LabelPersistentSecretContainer] = outputContainer.Name
+	if ig, ok := podutil.LookupEnv(outputContainer.Env, converter.EnvInstanceGroupName); ok {
+		secretLabels[ejv1.LabelInstanceGroup] = ig
+	}
+
+	if exjob.Spec.Output.Versioned {
+		// Use secretName as versioned secret name prefix: <secretName>-v<version>
+		err = po.CreateVersionSecret(exjob.GetName(), exjob.GetUID(), secretName, data, secretLabels, "created by extendedjob")
+		if err != nil {
+			return errors.Wrapf(err, "could not persist ejob's %s output to a secret", exjob.GetName())
+		}
+	} else {
+		secret.StringData = data
+		secret.Labels = secretLabels
+
+		_, err = po.clientSet.CoreV1().Secrets(po.namespace).Create(secret)
+
+		if err != nil {
+			if apierrors.IsAlreadyExists(err) {
+				// If it exists update it
+				_, err = po.clientSet.CoreV1().Secrets(po.namespace).Update(secret)
+				if err != nil {
+					return errors.Wrapf(err, "failed to update secret %s for container %s in pod %s.", secretName, outputContainer.Name, podName)
+				}
+			} else {
+				return errors.Wrapf(err, "failed to create secret %s for container %s in pod %s.", secretName, outputContainer.Name, podName)
+			}
+		}
+
+	}
+	return nil
+}
+
+// CreateVersionSecret create a versioned kubernetes secret given the data.
+func (po *PersistOutputInterface) CreateVersionSecret(ownerName string, ownerID types.UID, secretName string, secretData map[string]string, labels map[string]string, sourceDescription string) error {
+	currentVersion, err := getGreatestVersion(po.clientSet, po.namespace, secretName)
+	if err != nil {
+		return err
+	}
+
+	version := currentVersion + 1
+	labels[versionedsecretstore.LabelVersion] = strconv.Itoa(version)
+	labels[versionedsecretstore.LabelSecretKind] = versionedsecretstore.VersionSecretKind
+
+	generatedSecretName, err := versionedsecretstore.GenerateSecretName(secretName, version)
+	if err != nil {
+		return err
+	}
+
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      generatedSecretName,
+			Namespace: po.namespace,
+			Labels:    labels,
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion:         versionedsecretstore.LabelAPIVersion,
+					Kind:               "ExtendedJob",
+					Name:               ownerName,
+					UID:                ownerID,
+					BlockOwnerDeletion: util.Bool(false),
+					Controller:         util.Bool(true),
+				},
+			},
+			Annotations: map[string]string{
+				versionedsecretstore.AnnotationSourceDescription: sourceDescription,
+			},
+		},
+		StringData: secretData,
+	}
+
+	_, err = po.clientSet.CoreV1().Secrets(po.namespace).Create(secret)
+	return err
 }
 
 // fileExists checks if the file exists
@@ -120,7 +234,7 @@ func fileExists(filename string) bool {
 }
 
 // waitForFile waits for the file to be created
-func waitForFile(containerIndex int, filePath string, fileNotifyChannel chan<- int, errorChannel chan<- error, containerName string) {
+func waitForFile(containerIndex int, filePath string, fileNotifyChannel chan<- int, errorChannel chan<- error, containerName string, outputFilePathPrefix string) {
 
 	if fileExists(filePath) {
 		fileNotifyChannel <- containerIndex
@@ -154,136 +268,14 @@ func waitForFile(containerIndex int, filePath string, fileNotifyChannel chan<- i
 		}
 	}()
 
-	err = watcher.Add(filepath.Join("/mnt/quarks/", containerName))
+	err = watcher.Add(filepath.Join(outputFilePathPrefix, containerName))
 	if err != nil {
 		errorChannel <- err
 	}
 	<-done
 }
 
-// authenticateInCluster authenticates with the in cluster and returns the client
-func authenticateInCluster() (*kubernetes.Clientset, *versioned.Clientset, error) {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		return nil, nil, errors.Wrapf(err, "failed to authenticate with incluster config")
-	}
-
-	clientSet, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, nil, errors.Wrapf(err, "failed to create clientset with incluster config")
-	}
-
-	versionedClientSet, err := versioned.NewForConfig(config)
-	if err != nil {
-		return nil, nil, errors.Wrapf(err, "failed to create versioned clientset with incluster config")
-	}
-
-	return clientSet, versionedClientSet, nil
-}
-
-func createOutputSecret(outputContainer corev1.Container, namePrefix string, namespace string, podName string, clientSet *kubernetes.Clientset, exjob *ejv1.ExtendedJob) error {
-
-	secretName := namePrefix + outputContainer.Name
-
-	// Fetch json from file
-	filePath := filepath.Join("/mnt/quarks/", outputContainer.Name, "output.json")
-	file, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return errors.Wrapf(err, "unable to read file %s in container %s in pod %s", filePath, outputContainer.Name, podName)
-	}
-	var data map[string]string
-	err = json.Unmarshal([]byte(file), &data)
-	if err != nil {
-		return errors.Wrapf(err, "failed to convert output file %s into json for creating secret %s in pod %s",
-			filePath, secretName, podName)
-	}
-
-	// Create secret for the outputfile to persist
-	secret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      secretName,
-			Namespace: namespace,
-		},
-	}
-	secretLabels := exjob.Spec.Output.SecretLabels
-	if secretLabels == nil {
-		secretLabels = map[string]string{}
-	}
-	secretLabels[ejv1.LabelPersistentSecretContainer] = outputContainer.Name
-	if ig, ok := podutil.LookupEnv(outputContainer.Env, converter.EnvInstanceGroupName); ok {
-		secretLabels[ejv1.LabelInstanceGroup] = ig
-	}
-
-	if exjob.Spec.Output.Versioned {
-		// Use secretName as versioned secret name prefix: <secretName>-v<version>
-		err = createVersionSecret(clientSet, namespace, exjob.GetName(), exjob.GetUID(), secretName, data, secretLabels, "created by extendedjob")
-		if err != nil {
-			return errors.Wrapf(err, "could not persist ejob's %s output to a secret", exjob.GetName())
-		}
-	} else {
-		secret.StringData = data
-		secret.Labels = secretLabels
-
-		_, err = clientSet.CoreV1().Secrets(namespace).Create(secret)
-
-		if err != nil {
-			if apierrors.IsAlreadyExists(err) {
-				// If it exists update it
-				_, err = clientSet.CoreV1().Secrets(namespace).Update(secret)
-				if err != nil {
-					return errors.Wrapf(err, "failed to update secret %s for container %s in pod %s.", secretName, outputContainer.Name, podName)
-				}
-			} else {
-				return errors.Wrapf(err, "failed to create secret %s for container %s in pod %s.", secretName, outputContainer.Name, podName)
-			}
-		}
-
-	}
-	return nil
-}
-
-func createVersionSecret(clientSet *kubernetes.Clientset, namespace string, ownerName string, ownerID types.UID, secretName string, secretData map[string]string, labels map[string]string, sourceDescription string) error {
-	currentVersion, err := getGreatestVersion(clientSet, namespace, secretName)
-	if err != nil {
-		return err
-	}
-
-	version := currentVersion + 1
-	labels[versionedsecretstore.LabelVersion] = strconv.Itoa(version)
-	labels[versionedsecretstore.LabelSecretKind] = versionedsecretstore.VersionSecretKind
-
-	generatedSecretName, err := versionedsecretstore.GenerateSecretName(secretName, version)
-	if err != nil {
-		return err
-	}
-
-	secret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      generatedSecretName,
-			Namespace: namespace,
-			Labels:    labels,
-			OwnerReferences: []metav1.OwnerReference{
-				{
-					APIVersion:         versionedsecretstore.LabelAPIVersion,
-					Kind:               "ExtendedJob",
-					Name:               ownerName,
-					UID:                ownerID,
-					BlockOwnerDeletion: util.Bool(false),
-					Controller:         util.Bool(true),
-				},
-			},
-			Annotations: map[string]string{
-				versionedsecretstore.AnnotationSourceDescription: sourceDescription,
-			},
-		},
-		StringData: secretData,
-	}
-
-	_, err = clientSet.CoreV1().Secrets(namespace).Create(secret)
-	return err
-}
-
-func getGreatestVersion(clientSet *kubernetes.Clientset, namespace string, secretName string) (int, error) {
+func getGreatestVersion(clientSet kubernetes.Interface, namespace string, secretName string) (int, error) {
 	list, err := listSecrets(clientSet, namespace, secretName)
 	if err != nil {
 		return -1, err
@@ -304,7 +296,7 @@ func getGreatestVersion(clientSet *kubernetes.Clientset, namespace string, secre
 	return greatestVersion, nil
 }
 
-func listSecrets(clientSet *kubernetes.Clientset, namespace string, secretName string) ([]corev1.Secret, error) {
+func listSecrets(clientSet kubernetes.Interface, namespace string, secretName string) ([]corev1.Secret, error) {
 	secretLabelsSet := labels.Set{
 		versionedsecretstore.LabelSecretKind: versionedsecretstore.VersionSecretKind,
 	}
