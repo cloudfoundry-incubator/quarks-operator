f393c7b2b000c9311b799ce6918229f269fd4b06
Mario Manno <mario.manno@suse.com>
2019-12-13 12:01:58 +0100

    Revert "Remove versioned secret watch from generated_variable_controller"
    This reverts commit 0155d47095d8c7852923324cdbbc33c531a01237.

--- pkg/kube/controllers/boshdeployment/generated_variable_controller.go
+++ pkg/kube/controllers/boshdeployment/generated_variable_controller.go
@@ -3,6 +3,7 @@ package boshdeployment
 import (
 	"context"
 	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -64,7 +65,21 @@ func AddGeneratedVariable(ctx context.Context, config *config.Config, mgr manage
 		},
 		DeleteFunc:  func(e event.DeleteEvent) bool { return false },
 		GenericFunc: func(e event.GenericEvent) bool { return false },
-		UpdateFunc:  func(e event.UpdateEvent) bool { return false },
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			oldSecret := e.ObjectOld.(*corev1.Secret)
+			newSecret := e.ObjectNew.(*corev1.Secret)
+			shouldProcessEvent := isManifestWithOps(newSecret.Name) && !reflect.DeepEqual(oldSecret.Data, newSecret.Data)
+
+			if shouldProcessEvent {
+				ctxlog.NewPredicateEvent(newSecret).Debug(
+					ctx, e.MetaNew, bdv1.SecretReference,
+					fmt.Sprintf("Update predicate passed for %s, existing secret with the %s suffix has been updated",
+						e.MetaNew.GetName(), names.DeploymentSecretTypeManifestWithOps.String()),
+				)
+			}
+
+			return shouldProcessEvent
+		},
 	}
 
 	// This is a manifest with ops files secret that has changed.
