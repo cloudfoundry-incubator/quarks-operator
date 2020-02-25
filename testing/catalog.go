050294f07d4513780c76a498ca2082bab6c95c9a
Mario Manno <mario.manno@suse.com>
2020-02-25 12:52:45 +0100

    Use entanglement label to find secret
    * No longer sanitize the file name. It is configured in quarks job
      options
    
    [#171433464](https://www.pivotaltracker.com/story/show/171433464)

--- testing/catalog.go
+++ testing/catalog.go
@@ -21,6 +21,7 @@ import (
 	bdv1 "code.cloudfoundry.org/cf-operator/pkg/kube/apis/boshdeployment/v1alpha1"
 	"code.cloudfoundry.org/cf-operator/pkg/kube/controllers/statefulset"
 	bm "code.cloudfoundry.org/cf-operator/testing/boshmanifest"
+	qjv1 "code.cloudfoundry.org/quarks-job/pkg/kube/apis/quarksjob/v1alpha1"
 	"code.cloudfoundry.org/quarks-utils/pkg/config"
 	"code.cloudfoundry.org/quarks-utils/pkg/names"
 	"code.cloudfoundry.org/quarks-utils/pkg/pointers"
@@ -268,11 +269,13 @@ func (c *Catalog) DefaultConfigMap(name string) corev1.ConfigMap {
 
 // QuarksLinkSecret returns a link secret, as generated for consumption by an external (non BOSH) consumer
 func (c *Catalog) QuarksLinkSecret(deploymentName, linkType, linkName string, value map[string][]byte) corev1.Secret {
+	name := names.QuarksLinkSecretName(deploymentName, linkType, linkName)
 	return corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: names.QuarksLinkSecretName(deploymentName, linkType, linkName),
+			Name: name,
 			Labels: map[string]string{
 				manifest.LabelDeploymentName: deploymentName,
+				qjv1.LabelEntanglementKey:    name,
 			},
 		},
 		Data: value,
