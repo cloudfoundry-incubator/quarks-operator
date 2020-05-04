beaf09296825da4b340a9780958bf941adf14c3b
Ettore Di Giacinto <edigiacinto@suse.com>
2020-05-04 15:33:01 +0200

    Add MutatingWebhook to inject wait_for container
    The new PodMutator injects an InitContainer which waits for a service
    when the pod has the annotation "quarks.cloudfoundry.org/wait-for: 'service'".
    
    It creates an InitContainer which calls the operator
    to wait for the service to be in place.

---
+++ pkg/kube/controllers/waitservice/pod_webhook.go
@@ -0,0 +1,47 @@
+package waitservice
+
+import (
+	"go.uber.org/zap"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	admissionregistration "k8s.io/api/admissionregistration/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	wh "code.cloudfoundry.org/cf-operator/pkg/kube/util/webhook"
+	"code.cloudfoundry.org/quarks-utils/pkg/config"
+	"code.cloudfoundry.org/quarks-utils/pkg/names"
+)
+
+// NewWaitServicePodMutator returns a new webhook to inject init wait containers
+// to any pod with the "wait-for" annotation
+func NewWaitServicePodMutator(log *zap.SugaredLogger, config *config.Config) *wh.OperatorWebhook {
+	log.Info("Setting up mutator for injecting wait initcontainers to any pod")
+
+	mutator := NewPodMutator(log, config)
+
+	globalScopeType := admissionregistration.ScopeType("*")
+	return &wh.OperatorWebhook{
+		FailurePolicy: admissionregistration.Fail,
+		Rules: []admissionregistration.RuleWithOperations{
+			{
+				Rule: admissionregistration.Rule{
+					APIGroups:   []string{""},
+					APIVersions: []string{"v1"},
+					Resources:   []string{"pods"},
+					Scope:       &globalScopeType,
+				},
+				Operations: []admissionregistration.OperationType{
+					"CREATE",
+				},
+			},
+		},
+		Path: "/mutate-waiting-for-service-pods",
+		Name: "mutate-waiting-for-service-pods." + names.GroupName,
+		NamespaceSelector: &metav1.LabelSelector{
+			MatchLabels: map[string]string{
+				wh.LabelWatchNamespace: config.OperatorNamespace,
+			},
+		},
+		Webhook: &admission.Webhook{Handler: mutator},
+	}
+}
