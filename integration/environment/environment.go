c8772481150a779b4e06a454dbf4d575c731dc30
Mario Manno <mario.manno@suse.com>
2020-05-06 18:22:08 +0200

    Limit integration test operator to monitored namespaces

--- integration/environment/environment.go
+++ integration/environment/environment.go
@@ -45,16 +45,18 @@ const (
 func NewEnvironment(kubeConfig *rest.Config) *Environment {
 	atomic.AddInt32(&namespaceCounter, 1)
 	namespaceID := gomegaConfig.GinkgoConfig.ParallelNode*100 + int(namespaceCounter)
+	ns := utils.GetNamespaceName(namespaceID)
 
 	return &Environment{
 		Environment: &utils.Environment{
 			ID:         namespaceID,
-			Namespace:  utils.GetNamespaceName(namespaceID),
+			Namespace:  ns,
 			KubeConfig: kubeConfig,
 			Config: &config.Config{
 				CtxTimeOut:           10 * time.Second,
 				MeltdownDuration:     defaultTestMeltdownDuration * time.Second,
 				MeltdownRequeueAfter: defaultTestMeltdownRequeueAfter * time.Second,
+				MonitoredID:          ns,
 				Fs:                   afero.NewOsFs(),
 			},
 		},
