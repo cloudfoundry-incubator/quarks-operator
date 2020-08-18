42cfc07fa69790482413f5f35095253a6cd0541b
Mario Manno <mario.manno@suse.com>
2020-08-18 12:39:06 +0200

    Refactor bosh dns unit test

--- pkg/kube/util/boshdns/bosh_test.go
+++ pkg/kube/util/boshdns/bosh_test.go
@@ -19,11 +19,13 @@ import (
 	"code.cloudfoundry.org/quarks-operator/pkg/kube/util/boshdns"
 )
 
-const boshDNSAddOn = `
+const aliasAddon = `
 {
+  "name": "bosh-dns-aliases",
   "jobs": [
     {
       "name": "bosh-dns-aliases",
+      "release": "bosh-dns-aliases",
       "properties": {
         "aliases": [
           {
@@ -134,92 +136,92 @@ const boshDNSAddOn = `
             ]
           }
         ]
-      },
-      "release": "bosh-dns-aliases"
+      }
     }
-  ],
-  "name": "bosh-dns-aliases"
+  ]
 }
 `
 
-func loadAddOn() *manifest.AddOn {
-	var addOn manifest.AddOn
-	err := json.Unmarshal([]byte(boshDNSAddOn), &addOn)
-	if err != nil {
-		// This should never happen, because boshDNSAddOn is a constant
-		panic("Loading yaml failed")
-	}
-	return &addOn
-}
-
-var _ = Describe("BoshDomainNameService", func() {
+var _ = Describe("BOSHDomainNameService", func() {
 	Context("Apply", func() {
 		var (
 			client *cfakes.FakeClient
 			dns    boshdns.DomainNameService
 		)
 
-		BeforeEach(func() {
-			igs := manifest.InstanceGroups{
-				&manifest.InstanceGroup{Name: "scheduler", AZs: []string{"az1", "az2"}},
-				&manifest.InstanceGroup{Name: "diego-api", AZs: []string{"az1", "az2"}},
-				&manifest.InstanceGroup{Name: "bits", AZs: []string{"az1", "az2"}},
+		loadAddOn := func(addon string) *manifest.AddOn {
+			var addOn manifest.AddOn
+			err := json.Unmarshal([]byte(addon), &addOn)
+			if err != nil {
+				// This should never happen, because test data is valid
+				panic("Loading yaml failed")
 			}
-			var err error
-			dns, err = boshdns.NewBoshDomainNameService(loadAddOn(), igs)
-			Expect(err).NotTo(HaveOccurred())
+			return &addOn
+		}
 
-			client = &cfakes.FakeClient{}
-			// Needed to get along with CreateOrUpdate
-			client.GetCalls(func(context context.Context, nn types.NamespacedName, object runtime.Object) error {
-				return apierrors.NewNotFound(schema.GroupResource{}, nn.Name)
-			})
-		})
+		When("BOSHDNS Addon has aliases", func() {
+			BeforeEach(func() {
+				igs := manifest.InstanceGroups{
+					&manifest.InstanceGroup{Name: "scheduler", AZs: []string{"az1", "az2"}},
+					&manifest.InstanceGroup{Name: "diego-api", AZs: []string{"az1", "az2"}},
+					&manifest.InstanceGroup{Name: "bits", AZs: []string{"az1", "az2"}},
+				}
+				var err error
+				dns, err = boshdns.NewBoshDomainNameService(loadAddOn(aliasAddon), igs)
+				Expect(err).NotTo(HaveOccurred())
 
-		It("creates coredns resources and generates resources", func() {
-			counter := 0
-			err := dns.Apply(context.Background(), "default", client, func(object v1.Object) error {
-				counter++
-				return nil
-			})
-			Expect(err).NotTo(HaveOccurred())
-			Expect(client.CreateCallCount()).To(Equal(3))
-
-			By("calling setOwner", func() {
-				Expect(counter).To(Equal(3))
+				client = &cfakes.FakeClient{}
+				// Needed to get along with CreateOrUpdate
+				client.GetCalls(func(context context.Context, nn types.NamespacedName, object runtime.Object) error {
+					return apierrors.NewNotFound(schema.GroupResource{}, nn.Name)
+				})
 			})
 
-			By("creating a corefile configmap")
-			_, obj, _ := client.CreateArgsForCall(0)
-			cm, ok := obj.(*corev1.ConfigMap)
-			Expect(ok).To(BeTrue())
-			corefile, ok := cm.Data["Corefile"]
-			Expect(ok).To(BeTrue())
+			It("creates coredns resources and generates resources", func() {
+				counter := 0
+				err := dns.Apply(context.Background(), "default", client, func(object v1.Object) error {
+					counter++
+					return nil
+				})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(client.CreateCallCount()).To(Equal(3)) // configmap, deployment, service
 
-			By("checking for entries in corefile")
-			Expect(corefile).To(ContainSubstring(`
+				By("calling setOwner", func() {
+					Expect(counter).To(Equal(3))
+				})
+
+				By("creating a corefile configmap")
+				_, obj, _ := client.CreateArgsForCall(0)
+				cm, ok := obj.(*corev1.ConfigMap)
+				Expect(ok).To(BeTrue())
+				corefile, ok := cm.Data["Corefile"]
+				Expect(ok).To(BeTrue())
+
+				By("checking for entries in corefile")
+				Expect(corefile).To(ContainSubstring(`
 template IN A bits.service.cf.internal {
 	match ^(([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])\.)*bits\.service\.cf\.internal\.$
 	answer "{{ .Name }} 60 IN CNAME bits.default.svc."
 	upstream`))
-			Expect(corefile).To(ContainSubstring(`
+				Expect(corefile).To(ContainSubstring(`
 template IN AAAA bits.service.cf.internal {
 	match ^(([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])\.)*bits\.service\.cf\.internal\.$
 	answer "{{ .Name }} 60 IN CNAME bits.default.svc."
 	upstream`))
-			Expect(corefile).To(ContainSubstring(`
+				Expect(corefile).To(ContainSubstring(`
 template IN CNAME bbs1.service.cf.internal {
 	match ^bbs1\.service\.cf\.internal\.$
 	answer "{{ .Name }} 60 IN CNAME diego-api.default.svc."
 	upstream
 }`))
 
-			By("checking for entries which are missing an instance group, but do not use _ query")
-			Expect(corefile).To(ContainSubstring(`
+				By("checking for entries which are missing an instance group, but do not use _ query")
+				Expect(corefile).To(ContainSubstring(`
 template IN A uaa.service.cf.internal {
 	match ^(([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])\.)*uaa\.service\.cf\.internal\.$
 	answer "{{ .Name }} 60 IN CNAME uaa.default.svc."
 	upstream`))
+			})
 		})
 	})
 })
