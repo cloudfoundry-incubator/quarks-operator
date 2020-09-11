403560ea14b3ad2b423458497430898638d0b6e7
Mario Manno <mario.manno@suse.com>
2020-09-11 12:11:05 +0200

    Use podref package from quarks-utils

--- pkg/kube/util/reference/secret_references.go
+++ pkg/kube/util/reference/secret_references.go
@@ -10,6 +10,7 @@ import (
 
 	bdv1 "code.cloudfoundry.org/quarks-operator/pkg/kube/apis/boshdeployment/v1alpha1"
 	"code.cloudfoundry.org/quarks-operator/pkg/kube/util/withops"
+	"code.cloudfoundry.org/quarks-utils/pkg/podref"
 )
 
 // GetSecretsReferencedBy returns a list of all names for Secrets referenced by the object
@@ -19,7 +20,7 @@ func GetSecretsReferencedBy(ctx context.Context, client crc.Client, object inter
 	case bdv1.BOSHDeployment:
 		return getSecretRefFromBdpl(ctx, client, object)
 	case corev1.Pod:
-		return getSecretRefFromPod(object), nil
+		return podref.GetSecretRefFromPodSpec(object.Spec), nil
 	default:
 		return nil, errors.New("can't get secret references for unknown type; supported types are BOSHDeployment and QuarksStatefulSet")
 	}
@@ -56,51 +57,4 @@ func getSecretRefFromBdpl(ctx context.Context, client crc.Client, object bdv1.BO
 	}
 
 	return result, nil
-}
-
-func getSecretRefFromPod(object corev1.Pod) map[string]bool {
-	return getSecretRefFromPodSpec(object.Spec)
-}
-
-func getSecretRefFromPodSpec(object corev1.PodSpec) map[string]bool {
-	result := map[string]bool{}
-
-	// Look at all volumes
-	for _, volume := range object.Volumes {
-		if volume.VolumeSource.Secret != nil {
-			result[volume.VolumeSource.Secret.SecretName] = true
-		}
-	}
-
-	// Look at all init containers
-	for _, container := range object.InitContainers {
-		for _, envFrom := range container.EnvFrom {
-			if envFrom.SecretRef != nil {
-				result[envFrom.SecretRef.Name] = true
-			}
-		}
-
-		for _, envVar := range container.Env {
-			if envVar.ValueFrom != nil && envVar.ValueFrom.SecretKeyRef != nil {
-				result[envVar.ValueFrom.SecretKeyRef.Name] = true
-			}
-		}
-	}
-
-	// Look at all containers
-	for _, container := range object.Containers {
-		for _, envFrom := range container.EnvFrom {
-			if envFrom.SecretRef != nil {
-				result[envFrom.SecretRef.Name] = true
-			}
-		}
-
-		for _, envVar := range container.Env {
-			if envVar.ValueFrom != nil && envVar.ValueFrom.SecretKeyRef != nil {
-				result[envVar.ValueFrom.SecretKeyRef.Name] = true
-			}
-		}
-	}
-
-	return result
 }
